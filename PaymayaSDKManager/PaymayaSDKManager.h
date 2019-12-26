eeb5ec77c8cc40b3c14f2e8f80d2e57afe3be34c
john roque jorillo <jorillojohnroque@gmail.com>
2019-12-27 01:40:26 +0800

    Initial Commit

---
+++ PaymayaSDKManager/PaymayaSDKManager.h
@@ -0,0 +1,19 @@
+//
+//  PaymayaSDKManager.h
+//  PaymayaSDKManager
+//
+//  Created by John Roque Jorillo on 12/27/19.
+//  Copyright © 2019 jroque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaymayaSDKManager.
+FOUNDATION_EXPORT double PaymayaSDKManagerVersionNumber;
+
+//! Project version string for PaymayaSDKManager.
+FOUNDATION_EXPORT const unsigned char PaymayaSDKManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymayaSDKManager/PublicHeader.h>
+
+
